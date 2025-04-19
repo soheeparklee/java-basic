c86253cca2fecfabd0b98f0506d389c84ca6cdc5
soheeparklee <soheeparklee@gmail.com>
2025-04-19 13:07:13 +0200

    package

--- src/main/java/com/example/javabasic/pack/PackageMain1.java
+++ src/main/java/com/example/javabasic/pack/PackageMain1.java
@@ -1,11 +1,13 @@
 package com.example.javabasic.pack;
 
-import com.example.javabasic.pack.a.User;
+import com.example.javabasic.pack.a.*;
 
 public class PackageMain1 {
     public static void main(String[] args) {
         Data data = new Data();
 
-        com.example.javabasic.pack.a.User user = new User();
+        User user = new User();
+        com.example.javabasic.pack.b.User userB = new com.example.javabasic.pack.b.User();
+
     }
 }
---
+++ src/main/java/com/example/javabasic/pack/b/User.java
@@ -0,0 +1,7 @@
+package com.example.javabasic.pack.b;
+
+public class User {
+    public User(){
+        System.out.println("pack a package created");
+    }
+}
