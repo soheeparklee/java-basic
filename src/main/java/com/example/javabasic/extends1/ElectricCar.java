f734590e5091e2916de3056a8418888e058404fe
soheeparklee <soheeparklee@gmail.com>
2025-04-24 12:09:00 +0200

    before implementing extends

---
+++ src/main/java/com/example/javabasic/extends1/ElectricCar.java
@@ -0,0 +1,11 @@
+package com.example.javabasic.extends1;
+
+public class ElectricCar {
+    public void move(){
+        System.out.println("move car");
+    }
+
+    public void charge(){
+        System.out.println("charge electric car");
+    }
+}
---
+++ src/main/java/com/example/javabasic/extends1/GasCar.java
@@ -0,0 +1,11 @@
+package com.example.javabasic.extends1;
+
+public class GasCar {
+    public void move(){
+        System.out.println("move car");
+    }
+
+    public void fillUp(){
+        System.out.println("fill gas car");
+    }
+}
