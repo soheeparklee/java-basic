4aa3e0e04cf2f17268782c74103ec95f671786e2
soheeparklee <soheeparklee@gmail.com>
2025-04-26 19:04:43 +0200

    wo interface, ad car

---
+++ src/main/java/com/example/javabasic/polymorphism2/car0/Model3Car.java
@@ -0,0 +1,15 @@
+package com.example.javabasic.polymorphism2.car0;
+
+public class Model3Car {
+    public void startEngine(){
+        System.out.println("startEngine Model3Car");
+    }
+
+    public void offEngine(){
+        System.out.println("offEngine Model3Car");
+    }
+
+    public void pressAccelerator(){
+        System.out.println("pressAccelerator Model3Car");
+    }
+}
